142aee48303ef29b47e04d5b9c27bbf787a4c0da
ShevaVarya <shevchik.varvara@mail.ru>
2025-04-14 20:05:11 +0300

    TASK-0: подготовка UI и навигации

---
+++ app/src/main/java/io/github/shevavarya/avito_tech_internship/feature/settings/ui/SettingsFragment.kt
@@ -0,0 +1,23 @@
+package io.github.shevavarya.avito_tech_internship.feature.settings.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import io.github.shevavarya.avito_tech_internship.core.ui.BaseFragment
+import io.github.shevavarya.avito_tech_internship.databinding.FragmentSettingsBinding
+
+class SettingsFragment: BaseFragment<FragmentSettingsBinding>() {
+    override fun createViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentSettingsBinding {
+        TODO("Not yet implemented")
+    }
+
+    override fun initUi() {
+        TODO("Not yet implemented")
+    }
+
+    override fun observeData() {
+        TODO("Not yet implemented")
+    }
+}
